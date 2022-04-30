a08bd8535f1e11d39dccbc9c81edd5b0921ddf33
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2022-04-30 13:47:40 -0400

    Reset to .net6

---
+++ JeuDontOnEstLeHeros.Web.UI/Controllers/AventureController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JeuDontOnEstLeHeros.Web.UI.Controllers
+{
+    public class AventureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

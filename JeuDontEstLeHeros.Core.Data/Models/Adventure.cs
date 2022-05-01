adbbf57ee820bdde1c8d1d794622bc3028968e88
CMGeorges <61295187+CMGeorges@users.noreply.github.com>
2022-05-01 16:15:47 -0400

    Section 6

---
+++ JeuDontEstLeHeros.Core.Data/Models/Adventure.cs
@@ -0,0 +1,15 @@
+﻿namespace JeuDontEstLeHeros.Core.Data.Models
+{
+    public class Adventure
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        #endregion
+
+        #region Ctor
+
+        #endregion
+    }
+}
---
+++ JeuDontOnEstLeHeros.Web.UI/Controllers/AdventureController.cs
@@ -0,0 +1,27 @@
+﻿using JeuDontEstLeHeros.Core.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JeuDontOnEstLeHeros.Web.UI.Controllers
+{
+    public class AdventureController : Controller
+    {
+        private readonly ILogger<AdventureController> _logger;
+
+        public AdventureController(ILogger<AdventureController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.myTitle = "Adventures";
+            List<Adventure> adventureList = new List<Adventure>();
+
+            adventureList.Add(new Adventure() { Id=1,Title="My first adventure"});
+            adventureList.Add(new Adventure() { Id = 2, Title = "My Second adventure" });
+
+
+            return View(adventureList);
+        }
+    }
+}
